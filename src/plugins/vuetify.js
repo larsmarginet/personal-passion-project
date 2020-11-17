8b8187ca2b023a351875a6d8d747e07176eb9436
larsmarginet <lars.marginet@student.howest.be>
2020-11-17 10:57:18 +0100

    added color theme Close #4

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    iconfont: 'md',
+    theme: {
+        themes: {
+            light: {
+                primary: '#6FCF97',
+                secondary: '#333333',
+                success: '#3cd1c2',
+                info: '#ffaa2c',
+                error: '#EB5757',
+            },
+        },
+    },
 });
